aa576d9ab6977f6affb90db64fcbd2bb11dd6a20
Chaitanya140806 <83645040+Chaitanya140806@users.noreply.github.com>
2021-05-09 09:22:35 +0530

    y

--- commands/third language/konkani.js
+++ commands/third language/konkani.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: "kon",
     execute(message) {
-        message.channel.send('<@&839028940965281812> Konkani Class Has Been Started. Kindly Join The <#840532419798499338>')
+        if(message.member.roles.cache.some(role => role.name === 'TEACHER')){
+            message.channel.send('<@&839028940965281812> Konkani Class Has Been Started. Kindly Join The <#840532419798499338>')
+        }else (message.reply ('Please Behave Like A Student'));
     }
 }
